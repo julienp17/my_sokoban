ad7677b5fb3d43b669957e5b0b32e3bf46ea01d9
Julien Pause <julien.pause@epitech.eu>
2019-12-08 15:17:58 +0400

    Add USAGE message in usage.h

--- include/usage.h
+++ include/usage.h
@@ -11,4 +11,11 @@
     #define MY_EXIT_SUCCESS         0
     #define MY_EXIT_OPTION          192
     #define MY_EXIT_FAILURE         84
+
+    #define USAGE \
+    "Usage\n" \
+    "    ./my_sokoban map\n" \
+    "DESCRIPTION\n" \
+    "    map file representing the warehouse map, containing '#' for walls,\n" \
+    "        'P' for the player, 'X' for boxes, and '0' for storage locations.\n"
 #endif
