2de7123d22f23324151a357082af1566380f369b
Julien Pause <julien.pause@epitech.eu>
2019-12-08 17:55:57 +0400

    Call get_map_from_file in main

--- Makefile
+++ Makefile
@@ -14,7 +14,8 @@ MAIN_F		=	main.c
 SRC			= 	$(addprefix $(SRC_D), $(SRC_F))
 OBJ 		= 	$(SRC:.c=.o)
 SRC_D 		= 	src/
-SRC_F 		=	usage.c
+SRC_F 		=	usage.c \
+				get_map_from_file.c \
 
 SRC_UT   	=	$(addprefix $(SRC_UT_D), $(SRC_UT_F))
 OBJ_UT 	 	=	$(SRC_UT:.c=.o)
--- src/main.c
+++ src/main.c
@@ -5,16 +5,21 @@
 ** Main file for my_sokoban
 */
 
+#include <stddef.h>
 #include "usage.h"
+#include "file_manipulation.h"
 
 int main(int ac, char **av)
 {
     int exit_code = 0;
+    char **map = NULL;
 
     exit_code = check_args(ac, av);
     if (exit_code == MY_EXIT_OPTION)
         return (MY_EXIT_SUCCESS);
     else if (exit_code == MY_EXIT_FAILURE)
         return (MY_EXIT_FAILURE);
+    map = get_map_from_file(av[1]);
+    (void)map;
     return (MY_EXIT_SUCCESS);
 }
