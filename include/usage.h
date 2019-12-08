6780e8291fe6ea998c336d738986fa8cc42109f1
Julien Pause <julien.pause@epitech.eu>
2019-12-08 14:41:56 +0400

    Add usage.h with macros MY_EXIT_SUCCESS (0) and MY_EXIT_FAILURE(84)

---
+++ include/usage.h
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2019
+** my_sokoban
+** File description:
+** Header file for usage
+*/
+
+#ifndef USAGE_H_
+    #define USAGE_H_
+
+    #define MY_EXIT_SUCCESS         0
+    #define MY_EXIT_FAILURE         84
+#endif
