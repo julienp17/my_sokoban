2362e6258ef3191a9386f08b0a4952a1ad052d0f
Julien Pause <julien.pause@epitech.eu>
2019-12-08 14:29:25 +0400

    Add project skel

---
+++ src/main.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2019
+** my_sokoban
+** File description:
+** Main file for my_sokoban
+*/
+
+int main(int ac, char **av)
+{
+    (void)ac;
+    (void)av;
+    return (0);
+}
