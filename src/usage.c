53a974fd3de60cfd97a00b0e1a179c5ab9a78e50
Julien Pause <julien.pause@epitech.eu>
2019-12-08 15:28:52 +0400

    Print error message when there is an incorrect number of arguments

--- src/usage.c
+++ src/usage.c
@@ -10,8 +10,10 @@
 
 int check_args(int ac, char **av)
 {
-    if (ac != 2)
+    if (ac != 2) {
+        my_puterr(BAD_NB_ARGS_ERR_MSG);
         return (MY_EXIT_FAILURE);
+    }
     if (my_strcmp(av[1], "-h") == 0 || my_strcmp(av[1], "--help") == 0) {
         my_putstr(USAGE);
         return (MY_EXIT_OPTION);
