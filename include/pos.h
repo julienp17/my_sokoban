07719a2da15c1deb05e25a2e4914f7019dc4a71e
Julien Pause <julien.pause@epitech.eu>
2019-12-09 18:53:24 +0400

    Add pos data structure header file

---
+++ include/pos.h
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2019
+** my_sokoban
+** File description:
+** Header file for pos struct
+*/
+
+#ifndef POS_H_
+    #define POS_H_
+
+    typedef struct position {
+        int x;
+        int y;
+    } pos_t;
+
+#endif
