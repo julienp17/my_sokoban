5f4e802ecd95c1c3a5377169059077bd918878ec
Julien Pause <julien.pause@epitech.eu>
2019-12-09 18:53:10 +0400

    Add my_sokoban header file

---
+++ include/my_sokoban.h
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2019
+** my_sokoban
+** File description:
+** Header file for my_sokoban
+*/
+
+#ifndef MY_SOKOBAN_H
+    #define MY_SOKOBAN_H
+
+    #define WALL_CHAR       '#'
+    #define PLAYER_CHAR     'P'
+    #define TARGET_CHAR     'O'
+    #define SPACE_CHAR      ' '
+#endif
