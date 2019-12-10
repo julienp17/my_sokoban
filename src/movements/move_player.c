8ffbb53e766ff108d945895e228b420f187d7ee8
Julien Pause <julien.pause@epitech.eu>
2019-12-10 15:38:32 +0400

    Refactor get_initial_player_pos

--- src/movements/move_player.c
+++ src/movements/move_player.c
@@ -15,18 +15,16 @@
 #include "my_sokoban.h"
 #include "move.h"
 
-pos_t *get_initial_player_pos(char **map, char player_symbol)
+void get_initial_player_pos(char **map, pos_t *player_pos, char player_symbol)
 {
-    pos_t *player_pos = malloc(sizeof(pos_t *));
-
     for (unsigned int i = 0 ; map[i] ; i++) {
-        for (unsigned int j = 0 ; map[i][j] ; j++)
+        for (unsigned int j = 0 ; map[i][j] ; j++) {
             if (map[i][j] == player_symbol) {
                 player_pos->y = i;
                 player_pos->x = j;
             }
+        }
     }
-    return (player_pos);
 }
 
 void check_player_move(int key, map_t *map, pos_t *player_pos)
