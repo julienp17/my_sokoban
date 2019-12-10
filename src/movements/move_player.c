6c00551bccc4848a87c516572c702d6ccfae89f9
Julien Pause <julien.pause@epitech.eu>
2019-12-10 16:50:00 +0400

    Add move_player && move_box functions in move_player.c

--- src/movements/move_player.c
+++ src/movements/move_player.c
@@ -34,8 +34,37 @@ void check_player_move(int key, map_t *map, pos_t *player_pos)
     if (!is_movement_key(key))
         return;
     direction = get_move_by_key(key);
-    if (can_move(map, player_pos, direction)) {
-        player_pos->x += direction->x_offset;
-        player_pos->y += direction->y_offset;
+    if (can_move(map, player_pos, direction))
+        move_player(map->map, player_pos, direction);
+    free(direction);
+}
+
+void move_player(char **map, pos_t *player_pos, move_t *direction)
+{
+    char target_char = 0;
+
+    player_pos->x += direction->x_offset;
+    player_pos->y += direction->y_offset;
+    target_char = map[player_pos->y][player_pos->x];
+    if (target_char == BOX_CHAR)
+        move_box(map, player_pos, direction);
+}
+
+void move_box(char **map, pos_t *box_pos, move_t *direction)
+{
+    unsigned int nb_boxes = 0;
+    char target_char = map[box_pos->y][box_pos->x];
+
+    while (target_char == BOX_CHAR) {
+        nb_boxes++;
+        box_pos->x += direction->x_offset;
+        box_pos->y += direction->y_offset;
+        target_char = map[box_pos->y][box_pos->x];
+    }
+    for (unsigned int i = 0 ; i < nb_boxes ; i++) {
+        map[box_pos->y][box_pos->x] = BOX_CHAR;
+        box_pos->x -= direction->x_offset;
+        box_pos->y -= direction->y_offset;
+        map[box_pos->y][box_pos->x] = SPACE_CHAR;
     }
 }
