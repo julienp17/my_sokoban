bc59ca5a62bd481427840eacea053b0b538b1e06
Julien Pause <julien.pause@epitech.eu>
2019-12-10 13:18:31 +0400

    Refactor get_move to return a pointer to a struct move

--- src/get_move.c
+++ src/get_move.c
@@ -5,40 +5,27 @@
 ** Returns a movement structure based off a key
 */
 
+#include <stdlib.h>
 #include <curses.h>
 #include "move.h"
 
 move_t *get_move_by_key(int key)
 {
-    move_t *moves[] = {
-        {KEY_LEFT, -1, 0, &move_left},
-        {KEY_RIGHT, 1, 0, &move_right},
-        {KEY_UP, 0, -1, &move_up},
-        {KEY_DOWN, 0, 1, &move_down},
-        NULL
+    move_t *move = malloc(sizeof(*move));
+    move_t moves[] = {
+        {KEY_LEFT, -1, 0},
+        {KEY_RIGHT, 1, 0},
+        {KEY_UP, 0, -1},
+        {KEY_DOWN, 0, 1},
+        {0, 0, 0}
     };
-    for (unsigned int i = 0 ; moves[i] ; i++)
-        if (moves[i]->key == key)
-            return (moves);
-    return (NULL);
-}
 
-void move_left(pos_t *pos)
-{
-    pos->x--;
-}
-
-void move_right(pos_t *pos)
-{
-    pos->x++;
-}
-
-void move_up(pos_t *pos)
-{
-    pos->y--;
-}
-
-void move_down(pos_t *pos)
-{
-    pos->y++;
+    for (unsigned int i = 0 ; moves[i].key != 0 ; i++) {
+        if (moves[i].key == key) {
+            move->key = moves[i].key;
+            move->x_offset = moves[i].x_offset;
+            move->y_offset = moves[i].y_offset;
+        }
+    }
+    return (move);
 }
