907508909a040860a13739a665f64d428c16e675
Julien Pause <julien.pause@epitech.eu>
2019-12-10 16:49:36 +0400

    Add move_player && move_box function prototypes in move.h

--- include/move.h
+++ include/move.h
@@ -22,4 +22,6 @@
     bool can_move(map_t *map, pos_t *pos, move_t *move);
     bool is_off_limits(pos_t *pos, map_t *map);
     void check_player_move(int key, map_t *map, pos_t *player_pos);
+    void move_player(char **map, pos_t *player_pos, move_t *direction);
+    void move_box(char **map, pos_t *box_pos, move_t *direction);
 #endif
