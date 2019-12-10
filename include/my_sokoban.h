cbebcf832cd48c2cacdf0738de1b2efd5c9db3b0
Julien Pause <julien.pause@epitech.eu>
2019-12-10 18:21:10 +0400

    Add new macros QUIT_KEY set to 'q' and ESCAPE_KEY set to 27, and add game_logic functions prototypes in my_sokoban.h

--- include/my_sokoban.h
+++ include/my_sokoban.h
@@ -8,6 +8,7 @@
 #ifndef MY_SOKOBAN_H
     #define MY_SOKOBAN_H
 
+    #include <stdbool.h>
     #include <curses.h>
     #include "map.h"
     #include "pos.h"
@@ -17,10 +18,17 @@
     #define BOX_CHAR        'X'
     #define TARGET_CHAR     'O'
     #define SPACE_CHAR      ' '
-    #define RESET_KEY       ' '
 
-    void play_game(map_t *map);
+    #define RESET_KEY       ' '
+    #define QUIT_KEY        'q'
+    #define ESCAPE_KEY      27
+
+    int play_game(map_t *map);
     void init_game(void);
-    void game_loop(map_t *map, char **org_map, pos_t *player_pos);
+    int game_loop(map_t *map, char **org_map, pos_t *player_pos);
     void check_reset(int key, map_t *map, char **org_map, pos_t *player_pos);
+
+    bool game_should_go_on(int key, map_t *map);
+    bool no_boxes_can_be_moved(map_t *map);
+    unsigned int get_box_possible_moves(map_t *map, pos_t *pos);
 #endif
