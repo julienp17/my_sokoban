5891076b3e9effb9c7fcccdb970c2db4fc817e14
Julien Pause <julien.pause@epitech.eu>
2019-12-12 15:53:07 +0400

    Fix bug in my_count_char_2D_array.c

--- lib/my/my_count_char_2D_array.c
+++ lib/my/my_count_char_2D_array.c
@@ -10,7 +10,7 @@ unsigned int my_count_char_2D_array(char const **str, char const to_count)
     unsigned int count = 0;
 
     for (unsigned int i = 0 ; str[i] ; i++)
-        for (unsigned int j = 0 ; str[j] ; j++)
+        for (unsigned int j = 0 ; str[i][j] ; j++)
             if (str[i][j] == to_count)
                 count++;
     return (count);
