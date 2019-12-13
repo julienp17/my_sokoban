b5ad8a8035c2b0c985c62730c48522fdb1f9c118
Julien Pause <julien.pause@epitech.eu>
2019-12-13 18:26:38 +0400

    Rename file_buffer parameter to str in my_str_to_word_array

--- lib/my/my_str_to_word_array.c
+++ lib/my/my_str_to_word_array.c
@@ -9,9 +9,9 @@
 #include <stdlib.h>
 #include "my.h"
 
-char **my_str_to_word_array(char *file_buffer, char const sep)
+char **my_str_to_word_array(char *str, char const sep)
 {
-    unsigned int nb_rows = my_count_char(file_buffer, sep) + 1;
+    unsigned int nb_rows = my_count_char(str, sep) + 1;
     unsigned int nb_cols = 0;
     unsigned int i = 0;
     unsigned int j = 0;
@@ -19,10 +19,10 @@ char **my_str_to_word_array(char *file_buffer, char const sep)
     char **arr = malloc(sizeof(char *) * (nb_rows + 1));
 
     for (i = 0 ; i < nb_rows ; i = i + 1, k = k + 1) {
-        nb_cols = my_count_until_sep(file_buffer, sep);
+        nb_cols = my_count_until_sep(str, sep);
         arr[i] = malloc(sizeof(char) * (nb_cols + 1));
         for (j = 0 ; j < nb_cols ; j = j + 1, k = k + 1)
-            arr[i][j] = file_buffer[k];
+            arr[i][j] = str[k];
         arr[i][j] = '\0';
     }
     arr[i] = NULL;
