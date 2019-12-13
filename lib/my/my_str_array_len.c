d1e616833702b337b6efe3f72123fa7755790a1b
Julien Pause <julien.pause@epitech.eu>
2019-12-13 18:09:17 +0400

    Add my_str_array_len to libmy

---
+++ lib/my/my_str_array_len.c
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2019
+** my_sokoban
+** File description:
+** Returns the nuber of rows in a string array
+*/
+
+unsigned int my_str_array_len(char **arr)
+{
+    unsigned int len = 0;
+
+    while (arr[len]);
+        len++;
+    return (len);
+}
