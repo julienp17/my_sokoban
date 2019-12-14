9c4389383f131e336b9ef4a7ba1cbc04f316bfa2
Julien Pause <julien.pause@epitech.eu>
2019-12-14 22:30:35 +0400

    Refactor end_conditions entirely

--- src/end_conditions.c
+++ src/end_conditions.c
@@ -9,50 +9,59 @@
 #include <stdbool.h>
 #include "my_sokoban.h"
 #include "my.h"
+#include "move.h"
 
 int game_should_go_on(int key, map_t *map)
 {
     if (key == QUIT_KEY || key == ESCAPE_KEY)
         return (0);
-    // if (!boxes_can_be_moved(map))
-    //     return (0);
-    if (my_count_char_str_array((map->map), TARGET_CHAR) == 0)
+    if (all_storage_are_filled(map->storage, map->boxes))
+        return (0);
+    if (!all_boxes_can_be_moved(map))
         return (1);
     return (42);
 }
 
-// bool boxes_can_be_moved(map_t *map)
-// {
-//     pos_t *box_pos = malloc(sizeof(*box_pos));
-//     bool can_move = false;
+bool all_storage_are_filled(pos_t **storage, pos_t **boxes)
+{
+    bool filled = true;
 
-//     for (unsigned int row = 0 ; map->map[row] && !can_move; row++) {
-//         for (unsigned int col = 0 ; map->map[row][col] && !can_move; col++) {
-//             if (map->map[row][col] == BOX_CHAR) {
-//                 box_pos->y = row;
-//                 box_pos->x = col;
-//                 can_move = check_box_possible_moves(map, box_pos);
-//             }
-//         }
-//     }
-//     free(box_pos);
-//     return (can_move);
-// // }
+    for (unsigned int i = 0 ; storage[i] && filled ; i++)
+        filled = storage_is_filled(storage[i], boxes);
+    return (filled);
+}
 
-// bool check_box_possible_moves(map_t *map, pos_t *pos)
-// {
-//     int x_keys[] = {KEY_LEFT, KEY_RIGHT, 0};
-//     int y_keys[] = {KEY_UP, KEY_DOWN, 0};
-//     int x_possible_moves = 0;
-//     int y_possible_moves = 0;
+bool storage_is_filled(pos_t *storage_pos, pos_t **boxes)
+{
+    for (unsigned int i = 0 ; boxes[i] ; i++)
+        if (is_on_same_pos(storage_pos, boxes[i]))
+            return (true);
+    return (false);
+}
 
-//     for (unsigned int i = 0 ; x_keys[i] != 0 ; i++) {
-//         if (box_can_move(map, pos, get_move_by_key(x_keys[i])))
-//             x_possible_moves += 1;
-//         if (box_can_move(map, pos, get_move_by_key(y_keys[i])))
-//             y_possible_moves += 1;
-//     }
-//     if (x_possible_moves <= 1 && y_possible_moves <= 1)
-//         return (FALSE);
-//     return (TRUE);
-// }
+bool all_boxes_can_be_moved(map_t *map)
+{
+    bool can_move = false;
+
+    for (unsigned int i = 0 ; map->boxes[i] && !can_move ; i++)
+        can_move = box_can_move(map, map->boxes[i]);
+    return (can_move);
+}
+
+bool box_can_move(map_t *map, pos_t *pos)
+{
+    int x_keys[] = {KEY_LEFT, KEY_RIGHT, 0};
+    int y_keys[] = {KEY_UP, KEY_DOWN, 0};
+    int x_possible_moves = 0;
+    int y_possible_moves = 0;
+
+    for (unsigned int i = 0 ; x_keys[i] != 0 ; i++) {
+        if (can_move(map, pos, get_move_by_key(x_keys[i]), false))
+            x_possible_moves += 1;
+        if (can_move(map, pos, get_move_by_key(y_keys[i]), false))
+            y_possible_moves += 1;
+    }
+    if (x_possible_moves <= 1 && y_possible_moves <= 1)
+        return (FALSE);
+    return (TRUE);
+}
