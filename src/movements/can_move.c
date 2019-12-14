f90a30afa9fa791adfe02775621ff3d0675151c4
Julien Pause <julien.pause@epitech.eu>
2019-12-14 22:27:30 +0400

    Refactor can_move to make target inside function and fix bug in is_off_limits() x limit

--- src/movements/can_move.c
+++ src/movements/can_move.c
@@ -7,28 +7,26 @@
 
 #include <stdbool.h>
 #include <stdlib.h>
-#include <curses.h>
 #include "my_sokoban.h"
 #include "map.h"
 #include "pos.h"
 #include "move.h"
 #include "my.h"
 
-bool can_move(map_t *map, pos_t *target, move_t *direction, bool is_player)
+bool can_move(map_t *map, pos_t *pos, move_t *direction, bool is_player)
 {
-    pos_t *next_target = NULL;
+    pos_t *target = pos_create(pos->x + direction->x_offset,
+                                pos->y + direction->y_offset);
+    pos_t *box_pos = NULL;
 
     if (is_off_limits(target, map))
         return (FALSE);
     if (map->map[target->y][target->x] == WALL_CHAR)
         return (FALSE);
-    if ((next_target = get_box_pos(target, map->boxes)) != NULL) {
-        if (!is_player)
-            return (FALSE);
-        next_target->x = next_target->x + direction->x_offset;
-        next_target->y = next_target->y + direction->y_offset;
-        return (can_move(map, next_target, direction, false));
-    }
+    box_pos = get_box_pos(target, map->boxes);
+    if (box_pos)
+        return ((is_player) ? can_move(map, box_pos, direction, false) : FALSE);
+    free(target);
     return (TRUE);
 }
 
@@ -42,6 +40,6 @@ pos_t *get_box_pos(pos_t *target, pos_t **boxes)
 
 bool is_off_limits(pos_t *pos, map_t *map)
 {
-    return (pos->x > (int)my_strlen(map->map[pos->y])
-            || pos->y > (int)map->max_row);
+    return (pos->x > ((int)my_strlen(map->map[pos->y]) - 1)
+            || pos->y > ((int)map->max_row));
 }
