00998bd89a6d9622f6ed2bfa6ec372941f77083e
Julien Pause <julien.pause@epitech.eu>
2019-12-14 12:22:28 +0400

    Refactor move_player to work with pos_t and make it shorter

--- src/movements/move_player.c
+++ src/movements/move_player.c
@@ -15,44 +15,13 @@
 #include "my_sokoban.h"
 #include "move.h"
 
-void set_initial_player_pos(char **map, pos_t *player_pos, char player_symbol)
+void move_player(pos_t *player, pos_t **boxes, move_t *direction)
 {
-    for (unsigned int i = 0 ; map[i] ; i++) {
-        for (unsigned int j = 0 ; map[i][j] ; j++) {
-            if (map[i][j] == player_symbol) {
-                player_pos->y = i;
-                player_pos->x = j;
-            }
-        }
-    }
-}
+    pos_t *box_pos = NULL;
 
-void move_player(char **map, pos_t *player_pos, move_t *direction)
-{
-    char target_char = 0;
-
-    player_pos->x += direction->x_offset;
-    player_pos->y += direction->y_offset;
-    target_char = map[player_pos->y][player_pos->x];
-    if (target_char == BOX_CHAR)
-        move_box(map, player_pos, direction);
-}
-
-void move_box(char **map, pos_t *box_pos, move_t *direction)
-{
-    unsigned int nb_boxes = 0;
-    char target_char = map[box_pos->y][box_pos->x];
-
-    while (target_char == BOX_CHAR) {
-        nb_boxes++;
-        box_pos->x += direction->x_offset;
-        box_pos->y += direction->y_offset;
-        target_char = map[box_pos->y][box_pos->x];
-    }
-    for (unsigned int i = 0 ; i < nb_boxes ; i++) {
-        map[box_pos->y][box_pos->x] = BOX_CHAR;
-        box_pos->x -= direction->x_offset;
-        box_pos->y -= direction->y_offset;
-        map[box_pos->y][box_pos->x] = SPACE_CHAR;
-    }
+    player->x += direction->x_offset;
+    player->y += direction->y_offset;
+    box_pos = get_box_pos(player, boxes);
+    if (box_pos)
+        move_player(box_pos, boxes, direction);
 }
