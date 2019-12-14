ab754f585b36911432a9d9c7e8cbdb08139b52e1
Julien Pause <julien.pause@epitech.eu>
2019-12-14 22:34:06 +0400

    Change pos attributes from 'unsigned int' to 'int' and add pos related functions prototypes

--- include/pos.h
+++ include/pos.h
@@ -9,9 +9,13 @@
     #define POS_H_
 
     typedef struct position {
-        unsigned int x;
-        unsigned int y;
+        int x;
+        int y;
     } pos_t;
 
-    void set_initial_player_pos(char **map, pos_t *, char player_symbol);
+    pos_t *pos_create(int x, int y);
+    bool is_on_same_pos(pos_t *pos_1, pos_t *pos_2);
+    pos_t *get_box_pos(pos_t *target, pos_t **boxes);
+    pos_t *get_initial_player_pos(char **map, char symbol);
+    pos_t **get_initial_entities_pos(char **map, char symbol);
 #endif
