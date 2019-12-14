194078e30f1fbb9ae06eb5aafe2b1e9a2449e5eb
Julien Pause <julien.pause@epitech.eu>
2019-12-14 12:25:24 +0400

    Rename str to arr in my_count_char_str_array

--- lib/my/my_count_char_str_array.c
+++ lib/my/my_count_char_str_array.c
@@ -5,13 +5,13 @@
 ** Count the number of times the given char appears in a 2d array
 */
 
-unsigned int my_count_char_str_array(char const **str, char const to_count)
+unsigned int my_count_char_str_array(char **arr, char const to_count)
 {
     unsigned int count = 0;
 
-    for (unsigned int i = 0 ; str[i] ; i++)
-        for (unsigned int j = 0 ; str[i][j] ; j++)
-            if (str[i][j] == to_count)
+    for (unsigned int i = 0 ; arr[i] ; i++)
+        for (unsigned int j = 0 ; arr[i][j] ; j++)
+            if (arr[i][j] == to_count)
                 count++;
     return (count);
 }
